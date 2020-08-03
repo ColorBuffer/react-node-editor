a5a4b2fc30df93699da5a7eff38436329759be9b
nicoosokhan <nicoosokhan@yahoo.com>
2020-08-03 07:16:01 +0430

    f

--- src/dev/App.js
+++ src/dev/App.js
@@ -7,23 +7,62 @@ import Inverse from './logical/blocks/INVERSE'
 
 export default function App() {
 
-    const [blocks, setBlocks] = React.useState([])
-    const [positions, setPositions] = React.useState([])
+    const [blocks, setBlocks] = React.useState([
+        {
+            id: '1',
+            type: 'and',
+            title: 'and',
+            args: {
+
+            }
+        },
+        {
+            id: '2',
+            type: 'or',
+            title: 'or',
+            args: {
+
+            }
+        },
+        {
+            id: '3',
+            type: 'inverse',
+            title: 'inverse',
+            args: {
+
+            }
+        },
+    ])
+    const [positions, setPositions] = React.useState({
+        '1': {left: 0, top: 0},
+        '2': {left: 0, top: 0},
+        '3': {left: 0, top: 0},
+    })
 
     return (
         <div>
-            <And />
-            <Or />
-            <Inverse />
-            
             <NodeEditor
                 blocks={blocks}
                 setBlocks={setBlocks}
+                positions={positions}
+                setPositions={setPositions}
                 selectedBlocks={[]}
                 setSelectedBlocks={() => null}
-                renderBlock={() => (
-                    null
-                )}
+                renderBlock={({block}) => {
+                    if (block.type === 'and') {
+                        return (
+                            <And />
+                        )
+                    }
+                    if (block.type === 'or') {
+                        return (
+                            <Or />
+                        )
+                    }
+                    return (
+                        <Inverse />
+                    )
+                }}
                 renderPipe={({c1, c2, pipe}) => (
                     null
                 )}
